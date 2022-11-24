629259b737584bb9774a6c5ebb4383c538b7f8e3
dariRusAG <agapova.dr@students.dvfu.ru>
2022-11-24 20:38:36 +1000

    update

--- app.py
+++ app.py
@@ -7,3 +7,4 @@ import controllers.index
 import controllers.hello
 import controllers.subject
 import controllers.olymp
+import controllers.index_h
--- controllers/hello.py
+++ controllers/hello.py
@@ -35,15 +35,28 @@ def hello():
     # формируем список из выбранных пользователем олимпиад и конкурсов
     olymp_compet_select = [constants.olymp_compet[int(i)] for i in olymp_compet_id]
 
-    html = render_template('hello.html',
-                           name=name,
-                           gender=gender,
-                           program=constants.programs[int(program_id)],
-                           program_list=constants.programs,
-                           len=len,
-                           subjects_select=subjects_select,
-                           subject_list=constants.subjects,
-                           olymp_compet_select=olymp_compet_select,
-                           olymp_compet_list=constants.olymp_compet
-                           )
-    return html
+    html_hello = render_template('hello.html',
+                                 name=name,
+                                 gender=gender,
+                                 program=constants.programs[int(program_id)],
+                                 program_list=constants.programs,
+                                 len=len,
+                                 subjects_select=subjects_select,
+                                 subject_list=constants.subjects,
+                                 olymp_compet_select=olymp_compet_select,
+                                 olymp_compet_list=constants.olymp_compet
+                                 )
+
+    html_index = render_template('index_h.html',
+                                 name=name,
+                                 gender=gender,
+                                 program=constants.programs[int(program_id)],
+                                 program_list=constants.programs,
+                                 len=len,
+                                 subjects_select=subjects_select,
+                                 subject_list=constants.subjects,
+                                 olymp_compet_select=olymp_compet_select,
+                                 olymp_compet_list=constants.olymp_compet
+                                 )
+
+    return html_index + html_hello
